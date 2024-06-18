a77f16f40752deb3a7b7a7489327d29dd881c3b9
Rbaa02 <raphael.brandao0204@gmail.com>
2024-06-18 09:05:56 -0300

    JS

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,5 @@
+
+/*----------------- SWAP Pages Config -----------------*/
 const container = document.getElementById('container');
 const registerBtn = document.getElementById('register');
 const loginBtn = document.getElementById('login');
@@ -5,7 +7,64 @@ const loginBtn = document.getElementById('login');
 registerBtn.addEventListener('click', () => {
     container.classList.add("active");
 });
-
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
+
+
+/*----------------- Sing Up Config -----------------*/
+
+// variables
+const uNameIN = document.getElementById("uNameIN");
+const uNameUP = document.getElementById("uNameUP");
+
+const uEmail = document.getElementById("uEmail");
+
+const uPassIN = document.getElementById("uPassIN");
+const uPassUP = document.getElementById("uPassUP");
+
+const uCPass = document.getElementById("uCPass");
+const emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/;
+
+// dynamic event listeners
+uNameIN.addEventListener('input', () => {
+    validateName('IN');
+});
+uNameUP.addEventListener('input', () => {
+    validateName('UP');
+});
+uEmail.addEventListener('input', validateEmail);
+
+// uPass.addEventListener('input', validatePass);
+
+// uCPass.addEventListener('input', validateCPass);
+
+// functions
+function validateName(type){
+    let nameELEMENT = document.getElementById('uName' + type);
+    let name = nameELEMENT.value;
+
+    if(name === '' || name.length < 6){
+        nameELEMENT.style.border = "1px solid red";
+        // erro
+    }
+    else{
+        nameELEMENT.style.border = "1px solid green";
+    }
+}
+
+function validateEmail(){
+    let email = email.value;
+
+    if(email === ''){
+        uEmail.style.border = "1px solid red";
+        // erro
+    }
+    if(emailRegex.test(email)){
+        uEmail.style.border = "1px solid green";
+    }
+    else{
+        uEmail.style.border = "1px solid red";
+        // erro
+    }
+}
